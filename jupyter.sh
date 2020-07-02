d6cb83266e02d7e6e09e9cf11328f6f2a7aae950
Włodzimierz Kaoka <wlodzimierz@kaoka.pl>
2020-07-02 20:32:38 +0200

    notebook: training with gpu
    MAX_GPU_MEM set to fit my small but brave GF MX250 with 2GB mem
    still it gives around 5x speed up

--- jupyter.sh
+++ jupyter.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+while getopts ":g" opt; do
+  case ${opt} in
+    g) docker run --gpus all -it -p 8888:8888 -u $(id -u):$(id -g) -v $PWD/:/tf/work -w /tf/work tensorflow/tensorflow:latest-gpu-py3-jupyter
+      exit;;
+    \?) echo "Usage: cmd [-g]"
+      exit;;
+  esac
+done
+
 docker run -it -p 8888:8888 -u $(id -u):$(id -g) -v $PWD/:/tf/work -w /tf/work tensorflow/tensorflow:latest-py3-jupyter
