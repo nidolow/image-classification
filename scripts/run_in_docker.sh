8219126a0a7c2bee0e8dfe4f12748d68d1e96354
Włodzimierz Kaoka <wlodzimierz@kaoka.pl>
2020-07-05 14:06:43 +0200

    impriving docker run
    tensorflow image with gpu does not allow
    pip [--user] with custom user, so it is
    run with root

--- scripts/run_in_docker.sh
+++ scripts/run_in_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --it -v $PWD/:/tf/work -w /tf/work tensorflow/tensorflow:latest-py3 bash -c "pip install -r requirements.txt; $*"
+docker run -it -u $(id -u):$(id -u) -v $PWD/:/tf/work -w /tf/work tensorflow/tensorflow:latest-py3 bash -c "pip install -r requirements.txt; $*"
